cc017fd30ec3f61cc5e0e9b65ccddada0d5c8a9c
Daviny <daviny@vidal.dev.br>
2021-09-18 20:29:36 -0300

    deploy heroku

--- core/settings.py
+++ core/settings.py
@@ -172,6 +172,13 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
