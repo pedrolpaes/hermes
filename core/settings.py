1dfb208a2c904d38be48a7aeff8cc00a557adf04
Daviny <daviny@vidal.dev.br>
2021-10-09 20:42:57 -0300

    fix setting

--- core/settings.py
+++ core/settings.py
@@ -179,7 +179,12 @@ SIMPLE_JWT = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_ROOT = BASE_DIR / "static"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
+
 
